a715eecb6c8c5b9a0fc8a33c79b75125711462ea
Andrea D <nkjoep@gmail.com>
2015-11-12 22:25:17 +0100

    setup grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,67 @@
 module.exports = function(grunt) {
+  'use strict';
+  var fs = require('fs');
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    ejs: {
+      all: {
+        src: ['./src/template.ejs'],
+        dest: '<%= pkg.distPath %>/index.html',
+        options: {
+          data: JSON.parse(fs.readFileSync('./src/data.json')).data.info
+        }
+      }
+    },
+
+    copy: {
+      assets: {
+        expand: true,
+        cwd: './src/',
+        src: 'assets/**',
+        dest: '<%= pkg.distPath %>/'
+      }
+    },
+
+    clean: {
+      build: {
+        src: [
+          '<%= pkg.distPath %>/**/*'
+        ]
+      }
+    },
+
+    less: {
+      template: {
+        files: {
+          '<%= pkg.distPath %>/assets/app.css': './src/app.less'
+        }
+      }
+    },
+
+    watch: {
+      all: {
+        files: ['./src/**'],
+        tasks: ['build']
+      }
+    }
+  });
+
+
+  grunt.registerTask('dev', 'build the template in <%= pkg.distPath %> and watch for changes', function() {
+    grunt.task.run([
+      'clean', 'less:template', 'ejs', 'copy', 'watch'
+    ]);
+  });
+
+  grunt.registerTask('build', 'build the template in <%= pkg.distPath %>', function() {
+    grunt.task.run([
+      'clean', 'less:template', 'ejs', 'copy'
+    ]);
+  });
+
+  grunt.registerTask('default', ['build']);
 
 };
